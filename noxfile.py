d603d860440d9c290ce3926906f4ae28035932b0
Stig B. Dørmænen <stigbd@gmail.com>
2020-09-23 12:06:45 +0200

    Chore/add tests (#2)
    * chore: add basic contract testing
    
    * chore: add basic contract testing
    
    * Revert line split
    
    * Remove second build of docker image
    
    * Run tests only on 3.7
    
    * fix: minor rework of workflow files
    
    * chore: add basic contract testing
    
    * Revert line split
    
    * Remove second build of docker image
    
    * Run tests only on 3.7
    
    Co-authored-by: Hans Olav Sund <hans.olav.sund@digdir.no>

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ from nox.sessions import Session
 package = "fdk_harvester_bff"
 locations = "src", "tests", "noxfile.py"
 nox.options.stop_on_first_error = True
-nox.options.sessions = "lint", "mypy", "pytype"
+nox.options.sessions = "lint", "mypy", "pytype", "contract_tests"
 
 
 def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
@@ -25,7 +25,7 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def tests(session: Session) -> None:
     """Run the test suite."""
     args = session.posargs or ["--cov"]
@@ -34,7 +34,7 @@ def tests(session: Session) -> None:
     session.run("pytest", "-m unit", "-rA", *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def contract_tests(session: Session) -> None:
     """Run the contract_test suite."""
     args = session.posargs
@@ -43,7 +43,7 @@ def contract_tests(session: Session) -> None:
     session.run("pytest", "-m contract", *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def black(session: Session) -> None:
     """Run black code formatter."""
     args = session.posargs or locations
@@ -51,7 +51,7 @@ def black(session: Session) -> None:
     session.run("black", *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def lint(session: Session) -> None:
     """Lint using flake8."""
     args = session.posargs or locations
@@ -70,7 +70,7 @@ def lint(session: Session) -> None:
     session.run("flake8", *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     with tempfile.NamedTemporaryFile() as requirements:
@@ -87,7 +87,7 @@ def safety(session: Session) -> None:
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or locations
@@ -103,7 +103,7 @@ def pytype(session: Session) -> None:
     session.run("pytype", *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def xdoctest(session: Session) -> None:
     """Run examples with xdoctest."""
     args = session.posargs or ["all"]
@@ -112,7 +112,7 @@ def xdoctest(session: Session) -> None:
     session.run("python", "-m", "xdoctest", package, *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def docs(session: Session) -> None:
     """Build the documentation."""
     session.run("poetry", "install", "--no-dev", external=True)
@@ -120,7 +120,7 @@ def docs(session: Session) -> None:
     session.run("sphinx-build", "docs", "docs/_build")
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.7")
 def coverage(session: Session) -> None:
     """Upload coverage data."""
     install_with_constraints(session, "coverage[toml]", "codecov")
