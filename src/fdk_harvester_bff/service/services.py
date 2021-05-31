809599fed533b6831a3c44549affb7bb5e615619
NilsOveTen <github.nils.ove@tendenes.com>
2021-05-31 15:57:02 +0200

    feat: add dataservices

--- src/fdk_harvester_bff/service/services.py
+++ src/fdk_harvester_bff/service/services.py
@@ -2,8 +2,15 @@ from dataclasses import asdict
 from os import environ as env
 from typing import Any
 
-from fdk_rdf_parser import parse_concepts, parse_datasets, parse_information_models
+from fdk_rdf_parser import (
+    parse_concepts,
+    parse_data_services,
+    parse_datasets,
+    parse_information_models,
+)
 from requests import get, HTTPError
+
+from fdk_harvester_bff.enum.harvester_type import HarvesterType
 
 INFORMATION_MODEL_HARVESTER_BASE_URL = env.get(
     "INFORMATION_MODEL_HARVESTER_BASE_URL", "http://localhost:8000"
@@ -14,71 +21,30 @@ DATASET_HARVESTER_BASE_URL = env.get(
 CONCEPT_HARVESTER_BASE_URL = env.get(
     "CONCEPT_HARVESTER_BASE_URL", "http://localhost:8000"
 )
+DATA_SERVICE_HARVESTER_BASE_URL = env.get(
+    "DATA_SERVICE_HARVESTER_BASE_URL", "http://localhost:8000"
+)
 RECORDS_PARAM_TRUE = {"catalogrecords": "true"}
 
 
 def get_information_model_by_id(id: str) -> Any:
-    url = f"{INFORMATION_MODEL_HARVESTER_BASE_URL}/informationmodels/{id}"
-    try:
-        req = get(
-            url=url,
-            params=RECORDS_PARAM_TRUE,
-            headers={"Accept": "text/turtle"},
-            timeout=5,
-        )
-
-        if req.status_code == 404:
-            raise FetchFromServiceException(
-                status=404, reason=f"No information model with {id} found"
-            )
-        req.raise_for_status()
-
-        parsed_information_model = parse_information_models(req.text)
-        if parsed_information_model is None or len(parsed_information_model) != 1:
-            raise FetchFromServiceException(
-                status=500,
-                reason=f"Error when attempting to parse dataset with id {id}",
-            )
-        return asdict(
-            parsed_information_model.get(list(parsed_information_model.keys())[0])
-        )
-    except HTTPError as err:
-        raise FetchFromServiceException(status=500, reason=err.strerror)
-    except (ConnectionError, TimeoutError) as err:
-        raise FetchFromServiceException(status=502, reason=err.strerror)
+    return _get_and_parse_rdf_from_harvester(id, HarvesterType.INFO_MODEL)
 
 
 def get_dataset_by_id(id: str) -> Any:
-    url = f"{DATASET_HARVESTER_BASE_URL}/datasets/{id}"
-    try:
-        req = get(
-            url=url,
-            params=RECORDS_PARAM_TRUE,
-            headers={"Accept": "text/turtle"},
-            timeout=5,
-        )
-
-        if req.status_code == 404:
-            raise FetchFromServiceException(
-                status=404, reason=f"No dataset with {id} found"
-            )
-        req.raise_for_status()
-
-        parsed_dataset = parse_datasets(req.text)
-        if parsed_dataset is None or len(parsed_dataset) != 1:
-            raise FetchFromServiceException(
-                status=500,
-                reason=f"Error when attempting to parse dataset with id {id}",
-            )
-        return asdict(parsed_dataset.get(list(parsed_dataset.keys())[0]))
-    except HTTPError as err:
-        raise FetchFromServiceException(status=500, reason=err.strerror)
-    except (ConnectionError, TimeoutError) as err:
-        raise FetchFromServiceException(status=502, reason=err.strerror)
+    return _get_and_parse_rdf_from_harvester(id, HarvesterType.DATASET)
 
 
 def get_concept_by_id(id: str) -> Any:
-    url = f"{CONCEPT_HARVESTER_BASE_URL}/concepts/{id}"
+    return _get_and_parse_rdf_from_harvester(id, HarvesterType.CONCEPT)
+
+
+def get_data_service_by_id(id: str) -> Any:
+    return _get_and_parse_rdf_from_harvester(id, HarvesterType.DATA_SERVICE)
+
+
+def _get_and_parse_rdf_from_harvester(id: str, type: HarvesterType) -> Any:
+    url = _harvester_url(id, type)
     try:
         req = get(
             url=url,
@@ -89,21 +55,43 @@ def get_concept_by_id(id: str) -> Any:
 
         if req.status_code == 404:
             raise FetchFromServiceException(
-                status=404, reason=f"No concept with {id} found"
+                status=404, reason=f"No {type.value} with {id} found"
             )
         req.raise_for_status()
 
-        parsed_dataset = parse_concepts(req.text)
-        if parsed_dataset is None or len(parsed_dataset) != 1:
+        parsed = _parse_rdf(req.text, type)
+        if parsed is None or len(parsed) != 1:
             raise FetchFromServiceException(
                 status=500,
-                reason=f"Error when attempting to parse concept with id {id}",
+                reason=f"Error when attempting to parse {type.value} with id {id}",
             )
-        return asdict(parsed_dataset.get(list(parsed_dataset.keys())[0]))
+        return asdict(parsed.get(list(parsed.keys())[0]))
     except HTTPError as err:
         raise FetchFromServiceException(status=500, reason=err.strerror)
     except (ConnectionError, TimeoutError) as err:
         raise FetchFromServiceException(status=502, reason=err.strerror)
+
+
+def _harvester_url(id: str, type: HarvesterType) -> Any:
+    if type == HarvesterType.CONCEPT:
+        return f"{CONCEPT_HARVESTER_BASE_URL}/concepts/{id}"
+    elif type == HarvesterType.DATA_SERVICE:
+        return f"{DATA_SERVICE_HARVESTER_BASE_URL}/dataservices/{id}"
+    elif type == HarvesterType.DATASET:
+        return f"{DATASET_HARVESTER_BASE_URL}/datasets/{id}"
+    elif type == HarvesterType.INFO_MODEL:
+        return f"{INFORMATION_MODEL_HARVESTER_BASE_URL}/informationmodels/{id}"
+
+
+def _parse_rdf(rdf: str, type: HarvesterType) -> Any:
+    if type == HarvesterType.CONCEPT:
+        return parse_concepts(rdf)
+    elif type == HarvesterType.DATA_SERVICE:
+        return parse_data_services(rdf)
+    elif type == HarvesterType.DATASET:
+        return parse_datasets(rdf)
+    elif type == HarvesterType.INFO_MODEL:
+        return parse_information_models(rdf)
 
 
 class FetchFromServiceException(BaseException):
---
+++ tests/contract/test_dataservice.py
@@ -0,0 +1,26 @@
+"""Test cases for concepts."""
+from typing import Any
+
+import pytest
+import requests
+
+
+@pytest.mark.contract
+def test_get_data_service_with_id(http_service: Any) -> None:
+    test_id = "a7562855-d02c-3681-8c52-c41dae1c8edc"
+    url = f"{http_service}/dataservices/{test_id}"
+    result = requests.get(url=url, headers={"accept": "application/json"})
+    parsed_result = result.json()
+
+    assert parsed_result["id"] == test_id
+    assert (
+        parsed_result["uri"]
+        == "https://dataservice-catalog.staging.fellesdatakatalog.digdir.no/data-services/5f5f33a8cbf97c1bd3bdd8f6"
+    )
+    assert parsed_result["title"] == {"nb": "National Data Directory Search API"}
+    assert parsed_result["description"] == {
+        "nb": "Provides a basic search api against the National Data Directory of Norway"
+    }
+    assert parsed_result["endpointDescription"] == [
+        "https://raw.githubusercontent.com/brreg/openAPI/master/specs/fdk.json"
+    ]
